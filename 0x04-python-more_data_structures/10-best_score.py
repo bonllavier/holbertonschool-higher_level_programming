4c59a4d16a494097215bd1f88f9da92d97aa624f
javb92 <mentor37_@hotmail.com>
2019-05-10 01:58:30 +0000

    firs try

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is not None and a_dictionary:
+        x = sorted(a_dictionary, key=(lambda key: a_dictionary[key]),
+                   reverse=1)
+        return (x[0])
+    else:
         return None
-    x = sorted(a_dictionary, key=(lambda key: a_dictionary[key]), reverse=True)
-    return(x[0])
