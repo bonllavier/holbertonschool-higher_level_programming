71215329c468fb042dbca28bac78b096977ef3a8
javb92 <mentor37_@hotmail.com>
2019-05-10 19:09:43 +0000

    none fix

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda rows: list(map(lambda col: col ** 2, rows)),
-                    matrix))
+    return list(map(lambda r: list(map(lambda col: col ** 2, r)), matrix))
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if (type(roman_string) != str) or roman_string is None:
+        return 0
     count = 0
     tmp = 0
     dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
