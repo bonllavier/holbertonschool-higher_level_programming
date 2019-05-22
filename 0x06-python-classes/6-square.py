3531d5c09486f01d3b67dab83e504ccb11c0e730
javb92 <mentor37_@hotmail.com>
2019-05-22 03:05:36 +0000

    another commit

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @property
+    def position(self):
+        return self.__position
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @position.setter
+    def position(self, value):
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for iter in range(0, self.__size):
+                for iter3 in range(0, self.__position[0]):
+                    if self.__position[1] > 0:
+                        print("", end="")
+                    else:
+                        print(" ", end="")
+                for iter2 in range(0, self.__size):
+                    print("#", end="")
+                print("")
