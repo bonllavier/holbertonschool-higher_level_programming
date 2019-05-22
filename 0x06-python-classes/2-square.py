810e8527433539c8ceb92b7d4bf10e857974d1df
javb92 <mentor37_@hotmail.com>
2019-05-22 01:24:11 +0000

    third commit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,9 +2,12 @@
 class Square:
     def __init__(self, size=0):
         try:
-            size + 2
+            size += 0
         except TypeError:
             raise TypeError("size must be an integer")
-        if (size < 0):
+        try:
+            if (int(size) < 0):
+                raise ValueError
+        except ValueError:
             raise ValueError("size must be >= 0")
         self.__size = size
