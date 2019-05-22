81b6dd2c65ac7814dee43aa7b5333f4585ca64dd
javb92 <mentor37_@hotmail.com>
2019-05-22 01:39:38 +0000

    fix

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,13 +6,8 @@ class Square():
     """
     def __init__(self, size=0):
         """Initialization, only allowed type int > 0."""
-        try:
-            size + 2
-        except TypeError:
+        if type(size) != int:
             raise TypeError("size must be an integer")
-        try:
-            if (int(size) < 0):
-                raise ValueError
-            self.__size = size
-        except ValueError:
+        if (int(size) < 0):
             raise ValueError("size must be >= 0")
+        self.__size = size
