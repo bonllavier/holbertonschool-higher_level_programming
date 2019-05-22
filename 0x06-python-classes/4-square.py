ab5a361b2323ea88dcdc0d8da5432326c1f1cc0d
javb92 <mentor37_@hotmail.com>
2019-05-22 02:16:02 +0000

    first try

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
