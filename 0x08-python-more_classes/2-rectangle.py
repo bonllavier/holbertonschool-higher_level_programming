f78bf9b0e7cd54c0ed46344d49212cfbf1452e0e
javb92 <mentor37_@hotmail.com>
2019-05-28 17:29:58 +0000

    fixed

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -30,14 +30,14 @@ class Rectangle:
         """
         width getter
         """
-        return self.width
+        return self.__width
 
     @property
     def height(self):
         """
         height getter
         """
-        return self.height
+        return self.__height
 
     @width.setter
     def width(self, value):
