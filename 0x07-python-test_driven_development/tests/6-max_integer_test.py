434d16620478749fc1d7e4ecdd505a40d4b0b148
javb92 <mentor37_@hotmail.com>
2019-05-30 02:08:20 +0000

    fixes

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -13,32 +13,25 @@ class TestMaxInteger(unittest.TestCase):
         result = max_integer([1, 2, 3, 4])
         self.assertEqual(result, 4)
 
-    def test2(self):
+    def test_empty2_ver1(self):
         """
         simple cases2
         """
         result = max_integer([])
         self.assertEqual(result, None)
 
-    def test_None(self):
+    def test_nothing_ver1(self):
         """ test for 1 item
         """
         with self.assertRaises(TypeError):
             max_integer(None)
 
-    def test2(self):
+    def test_one_negative(self):
         """
         negative
         """
         with self.assertRaises(TypeError):
             max_integer(-1)
-
-    def conv_integer(self):
-        """
-        conversion cases
-        """
-        list = ["husjklsd", "sdjhklduhjiklsf;d"]
-        self.assertEqual(max_integer(list), 365)
 
     def test_false_integer(self):
         """
@@ -47,42 +40,42 @@ class TestMaxInteger(unittest.TestCase):
         list = [1, 2, 3, 4]
         self.assertFalse(max_integer(list) == 2)
 
-    def max_begin(self):
+    def test_max_begin(self):
         """
         max at the beginning
         """
         result = max_integer([4, 1, 2])
         self.assertEqual(result, 4)
 
-    def max_middle(self):
+    def test_max_middle(self):
         """
         max in middle
         """
         result = max_integer([1, 4, 2])
         self.assertEqual(result, 4)
 
-    def negative_number(self):
+    def test_negative_number(self):
         """
         negative in the list
         """
         result = max_integer([3, -1, 2])
         self.assertEqual(result, 3)
 
-    def all_negative(self):
+    def test_all_negative(self):
         """
         all negative
         """
         result = max_integer([-2, -3, -1])
-        self.assertEqual(result, None)
+        self.assertEqual(result, -1)
 
-    def only_one(self):
+    def test_only_one(self):
         """
         only one element
         """
         result = max_integer([2])
         self.assertEqual(result, 2)
 
-    def list_empty(self):
+    def test_list_empty(self):
         """
         list empty
         """
