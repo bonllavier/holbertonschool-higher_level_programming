78690bbf5e722776db4253e30ab885b4d986f355
javb92 <mentor37_@hotmail.com>
2019-05-30 03:08:55 +0000

    fixes

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+module 100-matrix_mul
+"""
+
+
+def matrix_mul(m_a, m_b):
+    """
+    matrix_mul
+    """
+    return m_a
---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+module 101-lazy_matrix_mul
+"""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    matrix
+    """
+    return m_a
