fcc92d50e9daabfda2a0ca43ad97fc578f3f422f
javb92 <mentor37_@hotmail.com>
2019-06-05 23:40:30 +0000

    second commit

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
+import sys
+import os.path
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-import sys
+"""
+module 9-add_item
+"""
+
+
 new_list = []
 filename = "add_item.json"
 for item in range(len(sys.argv)):
     if item > 0:
         new_list.append(sys.argv[item])
-#print(new_list)
-save_to_json_file(new_list, filename)
+if os.path.exists(filename):
+    my_list = load_from_json_file(filename)
+    for iter2 in new_list:
+        my_list.append(iter2)
+    save_to_json_file(my_list, filename)
+else:
+    save_to_json_file(new_list, filename)
