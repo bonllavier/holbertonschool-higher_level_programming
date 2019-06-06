474ee18ee7729490a22f9f09dc64b63ac318c731
javb92 <mentor37_@hotmail.com>
2019-06-06 00:23:33 +0000

    student exercise

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+class Student:
+    """
+    class student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        init method
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        to dict
+        """
+        return dict(
+            (key, value)
+            for (key, value) in self.__dict__.items())
