762839023523343478301ffcbe4402119fec0ebb
javb92 <mentor37_@hotmail.com>
2019-06-12 04:54:59 +0000

    advanced first try

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,7 @@ module base
 """
 import json
 import os.path
+import csv
 
 
 class Base:
@@ -81,3 +82,38 @@ class Base:
         else:
             pass
         return new_list
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """
+        save to file
+        """
+        filename = "{}.csv".format(cls.__name__)
+        if cls.__name__ == "Rectangle":
+            text = ["width", "height", "x", "y", "id"]
+        elif cls.__name__ == "Square":
+            text = ["size", "x", "y", "id"]
+
+        with open(filename, 'w') as file:
+            saver = csv.DictWriter(file, fieldnames=text)
+            if list_objs is not None:
+                saver.writeheader()
+                for text1 in list_objs:
+                    saver.writerow(text1.to_dictionary())
+            else:
+                saver.writerow([[]])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        filename = "{}.csv".format(cls.__name__)
+        new_list = []
+        if os.path.exists(filename):
+            with open(filename, newline="") as csvFile:
+                read = csv.DictReader(csvFile)
+                for row in read:
+                    for k, v in row.items():
+                        row[k] = int(v)
+                    new_list.append(row)
+            return [cls.create(**oj) for oj in new_list]
+        else:
+            return [[]]
