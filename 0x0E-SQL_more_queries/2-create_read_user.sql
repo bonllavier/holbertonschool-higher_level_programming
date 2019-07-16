089e098f692f3494b33b4a5e80923889265cb835
javb92 <mentor37_@hotmail.com>
2019-07-16 23:58:45 +0000

    firs try

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- 2. Read user
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON * . * TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
