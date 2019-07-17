7bcfdced73aa9af0d4cc731d96557f6d4021b6d1
javb92 <mentor37_@hotmail.com>
2019-07-17 01:07:01 +0000

    frist try

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- 7. Cities table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+     id INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+     state_id INT NOT NULL,
+     FOREIGN KEY (state_id) REFERENCES states(id),
+     name VARCHAR(256) NOT NULL
+);
