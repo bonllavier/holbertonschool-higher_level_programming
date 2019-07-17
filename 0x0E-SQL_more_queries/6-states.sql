9fd98919fa46bb3297f5771652eb7f79c8e0994c
javb92 <mentor37_@hotmail.com>
2019-07-17 00:50:48 +0000

    firs try

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- 6. States table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+     id INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+     name VARCHAR(256) NOT NULL
+);
