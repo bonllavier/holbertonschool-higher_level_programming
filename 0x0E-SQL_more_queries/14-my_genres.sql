f3e2a8eb4b9d252133dcac0498dafc673d21b41e
javb92 <mentor37_@hotmail.com>
2019-07-18 03:27:39 +0000

    fix

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,2 +1,2 @@
 -- 14. My genres
-SELECT tv_genres.name FROM tv_genres, tv_show_genres, tv_shows WHERE tv_genres.id IN (SELECT tv_show_genres.genre_id FROM tv_show_genres WHERE (SELECT tv_shows.id FROM tv_shows WHERE tv_shows.title = "Dexter") = tv_show_genres.show_id) GROUP BY tv_genres.name
+SELECT DISTINCT tv_genres.name FROM tv_genres, tv_shows, tv_show_genres WHERE tv_genres.id=tv_show_genres.genre_id and tv_show_genres.show_id = tv_shows.id and tv_shows.title = 'Dexter' ORDER BY tv_genres.name ASC;
