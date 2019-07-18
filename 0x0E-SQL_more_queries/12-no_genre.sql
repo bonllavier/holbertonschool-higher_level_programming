4a40b8b88c3b3781b5ec54f6c49eb8757ef8fe37
javb92 <mentor37_@hotmail.com>
2019-07-18 00:20:47 +0000

    firs test

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,2 @@
+-- 12 no genre
+SELECT DISTINCT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT OUTER JOIN tv_show_genres ON (tv_shows.id = tv_show_genres.show_id) WHERE tv_show_genres.genre_id IS NULL ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
