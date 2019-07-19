b728fc82eeef35bf85dd00c0cac3ba6a978679f9
javb92 <mentor37_@hotmail.com>
2019-07-19 02:13:06 +0000

    firs try

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,2 @@
+-- 16 show by genre
+SELECT tv_shows.title, tv_genres.name FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_genres.name ASC;
