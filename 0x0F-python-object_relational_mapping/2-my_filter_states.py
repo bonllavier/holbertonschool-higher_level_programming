2b32c2c2561665ce7aec4eef38a1d63f489b8e77
javb92 <mentor37_@hotmail.com>
2019-08-10 03:33:39 +0000

    firs try

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+module 2
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306)
+    cur = db.cursor()
+    consulta = (("SELECT * FROM states WHERE name = '{}'") +
+                (" GROUP BY states.name ORDER BY states.id ASC"))
+    numrows = cur.execute(consulta.format(sys.argv[4]))
+
+    for row in cur.fetchall():
+        print(row)
+    db.close()
---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+module 3
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306)
+    cur = db.cursor()
+    numrows = cur.execute("SELECT * FROM states WHERE name = %s\
+     GROUP BY states.name ORDER BY states.id ASC", (sys.argv[4],))
+    for row in cur.fetchall():
+        print(row)
+    db.close()
