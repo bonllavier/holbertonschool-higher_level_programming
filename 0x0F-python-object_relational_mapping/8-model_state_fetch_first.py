7bf9e4515d20d3f8a1f4b66d3bf94eb8768b7683
javb92 <mentor37_@hotmail.com>
2019-08-11 02:40:41 +0000

    first test

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -13,12 +13,15 @@ if __name__ == "__main__":
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     q = engine.execute("SELECT * FROM states ORDER BY states.id ASC LIMIT 1")
-    content = q.fetchall()
-    for iter in content:
-        for iter2 in iter:
-            if cont > 0:
-                print(": ", end='')
-            print(iter2, end='')
-            cont = cont + 1
-        cont = 0
-        print()
+    tmp_list = list(q)
+    if tmp_list is not None or len(tmp_list) == 0:
+        for iter in tmp_list:
+            for iter2 in iter:
+                if cont > 0:
+                    print(": ", end='')
+                print(iter2, end='')
+                cont = cont + 1
+            cont = 0
+            print()
+    else:
+        print("Nothing")
