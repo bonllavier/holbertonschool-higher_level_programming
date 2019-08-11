312398490b22b16b641e92902b702d90fcb560be
javb92 <mentor37_@hotmail.com>
2019-08-11 01:53:04 +0000

    first test

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+module 8
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    cont = 0
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    q = engine.execute("SELECT * FROM states ORDER BY states.id ASC LIMIT 1")
+    content = q.fetchall()
+    for iter in content:
+        for iter2 in iter:
+            if cont > 0:
+                print(": ", end='')
+            print(iter2, end='')
+            cont = cont + 1
+        cont = 0
+        print()
