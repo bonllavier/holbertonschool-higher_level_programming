a354d06657e40d5b277efabbefeb19bd3deaab06
javb92 <mentor37_@hotmail.com>
2019-08-21 01:43:05 +0000

    second test

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # 5- curl post
-curl --request POST "$1" -d 'email=hr@holbertonschool.com&subject=I will always be here for PLD'
+curl -s --request POST "$1" -d 'email=hr@holbertonschool.com&subject=I will always be here for PLD'
