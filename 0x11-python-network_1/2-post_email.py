05fdc5d1b22e7bb1b30e0e9326a693562cfe3143
javb92 <mentor37_@hotmail.com>
2019-08-27 03:23:27 +0000

    first test

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,10 +4,11 @@ import urllib.parse
 import urllib.request
 import sys
 
-url = sys.argv[1]
-values = {'email': sys.argv[2]}
-data = urllib.parse.urlencode(values)
-data = data.encode('ascii')
-req = urllib.request.Request(url, data)
-with urllib.request.urlopen(req) as response:
-    print(response.read().decode())
+if __name__ == '__main__':
+    url = sys.argv[1]
+    values = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode())
