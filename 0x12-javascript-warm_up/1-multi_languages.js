9b0ebd56e6ef62e979b7b1160efc3c583300557c
javb92 <mentor37_@hotmail.com>
2019-09-17 23:02:16 +0000

    [FIRST]

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myVar = 'C is fun';
+const myVar2 = 'Python is cool';
+const myVar3 = 'Javascript is amazing';
+console.log(myVar);
+console.log(myVar2);
+console.log(myVar3);
