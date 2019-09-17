795b8d69920cca1eef2b4c4aa44e0dff20b7ff21
javb92 <mentor37_@hotmail.com>
2019-09-17 23:18:37 +0000

    [FIX] added one missing condition

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-if (process.argv.slice(2).length > 0) {
+if (process.argv.slice(2).length > 1) {
+  console.log('Arguments found');
+} else if (process.argv.slice(2).length === 1) {
   console.log('Argument found');
 } else {
   console.log('No argument');
