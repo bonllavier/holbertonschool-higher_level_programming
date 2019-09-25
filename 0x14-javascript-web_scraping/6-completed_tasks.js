9e0bc3b4c61b2b67fd05087946225efebba6b35e
javb92 <mentor37_@hotmail.com>
2019-09-25 00:25:44 +0000

    [FIX] unorded dictionary

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,21 +4,28 @@ const request = require('request');
 let x = 0;
 const dict = {};
 let cont = 0;
-let userant = 0;
 let result = [];
 request(array[0], function (err, res, body) {
   if (err) {
-  }
-  result = JSON.parse(body);
-  for (x = 0; x < result.length; x++) {
-    if (userant !== result[x].userId) {
-      cont = 0;
+  } else {
+    try {
+      result = JSON.parse(body);
+      for (x = 0; x < result.length; x++) {
+        dict[result[x].userId.toString()] = 0;
+      }
+      for (var key in dict) {
+        cont = 0;
+        for (x = 0; x < result.length; x++) {
+          if (key === result[x].userId.toString() &&
+result[x].completed === true) {
+            cont = cont + 1;
+            dict[result[x].userId.toString()] = cont;
+          }
+        }
+      }
+      console.log(dict);
+    } catch (e) {
+      console.log(e);
     }
-    if (result[x].completed === true) {
-      cont = cont + 1;
-      dict[result[x].userId.toString()] = cont;
-    }
-    userant = result[x].userId;
   }
-  console.log(dict);
 });
