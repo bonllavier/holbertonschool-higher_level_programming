499980fe927ce4b43b1dbb8c9511fdf54b029a7b
javb92 <mentor37_@hotmail.com>
2019-09-25 03:32:36 +0000

    [FIX] dont show the key with 0 in value

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,10 +3,15 @@ const array = process.argv.slice(2);
 const request = require('request');
 let x = 0;
 const dict = {};
+const dict2 = {};
 let cont = 0;
 let result = [];
 let k2 = '';
+/* const fs = require('fs');
+const data = fs.readFileSync(array[0], 'utf8');
+*/
 request(array[0], function (err, res, body) {
+  result = JSON.parse(body);
   result = JSON.parse(body);
   if (err) {
     console.log(err);
@@ -20,10 +25,10 @@ request(array[0], function (err, res, body) {
         k2 = result[x].userId.toString();
         if (key === k2 && result[x].completed === true) {
           cont = cont + 1;
-          dict[result[x].userId.toString()] = cont;
+          dict2[result[x].userId.toString()] = cont;
         }
       }
     }
-    console.log(dict);
+    console.log(dict2);
   }
 });
