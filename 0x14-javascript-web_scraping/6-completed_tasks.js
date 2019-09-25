39128f3ae5fe7c0c1f25b18cfc4a71001b1dc701
javb92 <mentor37_@hotmail.com>
2019-09-25 00:32:21 +0000

    [FIX] semistandard

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -9,24 +9,20 @@ let k2 = '';
 request(array[0], function (err, res, body) {
   if (err) {
   } else {
-    try {
-      result = JSON.parse(body);
+    result = JSON.parse(body);
+    for (x = 0; x < result.length; x++) {
+      dict[result[x].userId.toString()] = 0;
+    }
+    for (var key in dict) {
+      cont = 0;
       for (x = 0; x < result.length; x++) {
-        dict[result[x].userId.toString()] = 0;
-      }
-      for (var key in dict) {
-        cont = 0;
-        for (x = 0; x < result.length; x++) {
-          k2 = result[x].userId.toString();
-          if (key === k2 && result[x].completed === true) {
-            cont = cont + 1;
-            dict[result[x].userId.toString()] = cont;
-          }
+        k2 = result[x].userId.toString();
+        if (key === k2 && result[x].completed === true) {
+          cont = cont + 1;
+          dict[result[x].userId.toString()] = cont;
         }
       }
-      console.log(dict);
-    } catch (e) {
-      console.log(e);
     }
+    console.log(dict);
   }
 });
